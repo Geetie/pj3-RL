8479e3832db50d5a75c9dc8ceab366aea9c0da03
2301_79378637 <2957338852@qq.com>
2026-06-12 20:40:48 +0800

    fix(record_video): 限制录制视频的episode数量
    修改了episode_trigger条件，改为仅录制前args.num_episodes个回合的视频

--- record_video.py
+++ record_video.py
@@ -37,7 +37,7 @@ def make_env(env_id, seed, idx, capture_video, run_name):
     def thunk():
         if capture_video and idx == 0:
             env = gym.make(env_id, render_mode="rgb_array", frameskip=1)
-            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}", episode_trigger=lambda x: True)
+            env = gym.wrappers.RecordVideo(env, f"videos/{run_name}", episode_trigger=lambda x: x < args.num_episodes)
         else:
             env = gym.make(env_id, frameskip=1)
 
